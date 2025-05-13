4948d3af59fe6b210f388e081eee5ce5cc52eb9b
Rodrigo <rjcm1209.06@gmail.com>
2025-05-13 09:05:55 -0600

    register de clientes terminado

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cookieParser from "cookie-parser";
 
 //routes
+import registerClientsRoute from "./src/routes/registerClientsRoute.js"
 
 
 
@@ -9,5 +10,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+//routes
+app.use("/api/registerClients", registerClientsRoute);
 
 export default app;
---
+++ backend/src/routes/registerClientsRoute.js
@@ -0,0 +1,10 @@
+import express from "express";
+import registerClientsController from "../controllers/registerClientsCtrl.js";
+
+const router = express.Router();
+
+router.route("/").post(registerClientsController.register);
+router.route("/verifyCodeEmail").post(registerClientsController.verificationCode);
+
+
+export default router;
